881e5340b106c33c26d14d5fc527f2df1c7a28a3
Oonie11 <utkarshpatel002@gmail.com>
2022-09-25 13:53:38 -0400

    add complete game foundation

--- script.js
+++ script.js
@@ -3,11 +3,16 @@
 const rock = "rock".toUpperCase();
 const paper = "paper".toUpperCase();
 const scissor = "scissor".toUpperCase();
+//SCORE CACHE
+let playerScore = 0;
+let computerScore = 0;
+let getScore = null;
+let gameRound = 1;
 
 //function to get computers choice
 function getComputerChoice() {
   // creating an array for computer to choose from.
-  const computerChoice = ["Rock", "Paper", "Scissor"];
+  const computerChoice = [rock, paper, scissor];
   //generating a random number to grab selection
   const random = Math.floor(Math.random() * computerChoice.length);
   return computerChoice[random];
@@ -20,15 +25,12 @@ const getPlayerChoice = () => {
   return answer;
 };
 
-//storing the answers of both player and computer
-const playerAnswer = getPlayerChoice();
-const computerInput = getComputerChoice().toUpperCase();
-
 function verifyAnswer(choice) {
   if (choice !== rock) {
     if (choice !== paper) {
       if (choice !== scissor) {
         alert("not Valid selection");
+        getScore = null;
         return getPlayerChoice();
       } else {
         return choice;
@@ -41,8 +43,6 @@ function verifyAnswer(choice) {
   }
 }
 
-const playerInput = verifyAnswer(playerAnswer);
-
 //function to play One round of the game
 function playOneRound(playerSelection, computerSelection) {
   //log players choice
@@ -50,19 +50,20 @@ function playOneRound(playerSelection, computerSelection) {
   console.log("Computer: " + computerSelection);
   // function to declare loose
   function youLoose() {
-    return console.log(
+    console.log(
       computerSelection + " beats " + playerSelection + " ,You loose!"
     );
+    return (getScore = false);
   }
   // function to declare win
   function youWin() {
-    return console.log(
-      playerSelection + " beats " + computerSelection + " ,You Win!"
-    );
+    console.log(playerSelection + " beats " + computerSelection + " ,You Win!");
+    return (getScore = true);
   }
   // function to declare draw
   function draw() {
-    return console.log("its a draw");
+    console.log("its a draw");
+    return (getScore = null);
   }
 
   // conditional statement to get result
@@ -77,16 +78,41 @@ function playOneRound(playerSelection, computerSelection) {
   }
 }
 
+//function to store calculate score
+function scoreBoard(score) {
+  if (score === null) {
+    playerScore++;
+    computerScore++;
+  } else if (score) {
+    playerScore++;
+  } else {
+    computerScore++;
+  }
+}
+
 //function to play multiple round of games
-const game = (player, computer) => {
-  //SCORE CACHE
-  const playerScore = 0;
-  const computerScore = 0;
-  playOneRound(player, computer);
-  //loop of 5 rounds
+const game = () => {
   for (let i = 0; i < 5; i++) {
-    console.log(i);
+    console.log("ROUND: " + gameRound + " Choose your Option");
+    //storing the answers of both player and computer
+    const playerAnswer = getPlayerChoice();
+    const computerInput = getComputerChoice();
+    const playerInput = verifyAnswer(playerAnswer);
+
+    playOneRound(playerInput, computerInput);
+    scoreBoard(getScore);
+    console.log("playerScore: " + playerScore);
+    console.log("computerScore: " + computerScore);
+    gameRound++;
+  }
+
+  if (playerScore === computerScore) {
+    console.log("the match is draw");
+  } else if (playerScore > computerScore) {
+    console.log("the winner of this game is  player");
+  } else {
+    console.log("the winner of this game is computer");
   }
 };
 
-game(playerInput, computerInput);
+game();
